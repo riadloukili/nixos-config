e5e198ded8e2da9362b77f1d50cb1c547e3badbe
Riad Loukili <me@riadloukili.com>
2025-06-24 16:31:54 +0000

    added grub boot device

--- hosts/hetzner/eu-lite-nix-1/configuration.nix
+++ hosts/hetzner/eu-lite-nix-1/configuration.nix
@@ -9,6 +9,10 @@
   time.timeZone         = "America/Toronto";
   i18n.defaultLocale    = "en_US.UTF-8";
 
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" "ext4" ];
+
   users.users.riad.extraGroups  = [ "wheel" ];
 
   security.sudo.wheelNeedsPassword = false;
