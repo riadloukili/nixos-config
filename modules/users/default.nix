ee2f66942550201a5b83ffbac78ea9ff733a1cfe
Riad Loukili <me@riadloukili.com>
2025-06-24 16:26:48 +0000

    trying fix #5

--- modules/users/default.nix
+++ modules/users/default.nix
@@ -1,16 +1,13 @@
 { config, lib, pkgs, ... }:
 
 let
-  entries   = builtins.readDir ./.;
-  paths     = lib.attrValues entries;
-  nixFiles  = lib.filter (path:
-    builtins.match ".*\\.nix$" (builtins.basename path) != null
-  ) paths;
-  userFiles = lib.filter (path:
-    builtins.basename path != "default.nix"
-  ) nixFiles;
+  dir       = ./.;
+  entries   = builtins.readDir dir;
+  userFiles = lib.filter
+    (f: f != "default.nix" && lib.strings.hasSuffix ".nix" f)
+    (builtins.attrNames entries);
 in
 
-lib.foldl' (acc: thePath:
-  acc // import thePath
+lib.foldl' (acc: name:
+  acc // import (dir + "/${name}")
 ) {} userFiles
