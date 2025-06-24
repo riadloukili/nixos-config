5d66af9d82fd1406ea04689fba77a9226d4b680e
Riad Loukili <me@riadloukili.com>
2025-06-24 16:09:34 +0000

    corrected user agg file

--- modules/users/default.nix
+++ modules/users/default.nix
@@ -1,7 +1,13 @@
-{ lib, ... }:
+{ config, lib, pkgs, ... }:
 
 let
-  allFiles = builtins.attrNames (builtins.readDir .);
-  userFiles = lib.filter (f: f != "default.nix" && lib.strings.hasSuffix ".nix" f) allFiles;
+  thisDir   = ./.
+  allFiles   = builtins.attrNames (builtins.readDir thisDir);
+  userFiles  = lib.filter (f: f != "default.nix" && lib.strings.hasSuffix ".nix" f) allFiles;
 in
-  lib.foldl' (acc: name: acc // import ./${name}) {} userFiles
+  lib.foldl' (acc: name:
+    let
+      modulePath = toString thisDir + "/" + name;
+    in
+      acc // import modulePath
+  ) {} userFiles
